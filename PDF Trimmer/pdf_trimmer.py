664aeb164d3bdf8d74c8477249e5dcf39f3391ed
Jaladh Singhal <jaladhsinghal@gmail.com>
2020-01-25 19:08:37 +0530

    Some improvents in code

--- PDF Trimmer/pdf_trimmer.py
+++ PDF Trimmer/pdf_trimmer.py
@@ -15,7 +15,7 @@ else:   # Replace the same file
 
 
 # Initialize PDF reader & writer objects
-infile = PyPDF2.PdfFileReader(in_fpath, 'rb') 
+in_file = PyPDF2.PdfFileReader(in_fpath, 'rb') 
 out_file = PyPDF2.PdfFileWriter()
 
 # To extract text from a PDF page
@@ -26,21 +26,21 @@ def extract_text(pageObj):
     return re.sub('[\n\r\s]+', '', text)
 
 
-prev_pg_text = extract_text(infile.getPage(0))
+prev_pg_text = extract_text(in_file.getPage(0))
 del_pages = []
 
-for pgNo in range(1, infile.numPages):
-    pg_text = extract_text(infile.getPage(pgNo))
+for pgNo in range(1, in_file.numPages):
+    pg_text = extract_text(in_file.getPage(pgNo))
     # If current page contains all text of previous page
     if pg_text.startswith(prev_pg_text):
         del_pages.append(pgNo-1) # Delete previous page
     prev_pg_text = pg_text
 
 # To delete pages, have to write a new PDF excluding those
-for pgNo in range(infile.numPages):
+for pgNo in range(in_file.numPages):
     if pgNo not in del_pages:
-        pg = infile.getPage(pgNo)
+        pg = in_file.getPage(pgNo)
         out_file.addPage(pg)
   
-with open('newfile.pdf', 'wb') as f:
+with open(out_fpath, 'wb') as f:
     out_file.write(f)
